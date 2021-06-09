0d4c75cc158d6919309d5c0bf59d5c13f1c82d55
danil fateev <rfney234@gmail.com>
2021-06-09 18:50:22 +0600

    costel

--- js/script.js
+++ js/script.js
@@ -2,10 +2,58 @@ $(document).ready(function(){
 $('.slider').slick({
   arrows:false,
   slidesToShow:6,
-  slidesToScroll:1,
-  touchThreshold:8,
+  slidesToScroll:3,
+  touchThreshold:10,
   waitForAnimate:false,
-  centerMode:false,
+  centerMode:true,
+  variableWidth: true,
+  infinite:true,
+  speed:300,
+  responsive: [
+    {
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+centerMode:true,
+      }
+     
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+centerMode:true,
+      }
+     
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+centerMode:true,
+      }
+     
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        centerMode:true,
+      }
+    },
+    {
+      breakpoint: 430,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode:true,
+      }
+    },
+  ]
 });
 $('.sliderbig').slick({
   arrows:true,
@@ -14,7 +62,38 @@ $('.sliderbig').slick({
   touchThreshold:8,
   waitForAnimate:false,
 centerMode:true,
-adaptiveHeight:true
+adaptiveHeight:true,
+variableWidth: true,
+responsive:[
+  {
+    breakpoint: 1100,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      centerMode:true,
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2,
+centerMode:true,
+  }
+},
+ {
+    breakpoint: 600,
+    settings: {
+      arrows:false,
+  }
+},
+{
+  breakpoint: 330,
+  settings: {
+    arrows:true,
+}
+},
+]
 });
 $('.slider_comments').slick({
   arrows:true,
@@ -23,6 +102,25 @@ $('.slider_comments').slick({
   touchThreshold:8,
   waitForAnimate:false,
 centerMode:true,
-adaptiveHeight:true
+adaptiveHeight:true,
+variableWidth:true,
+responsive:[
+  {
+    breakpoint: 1100,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      centerMode:true,
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2,
+centerMode:true,
+  }
+},
+]
 });
 });
